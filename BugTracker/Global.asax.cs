389ad2a0b08684256f91915cd5ce49edfff9cc00
chenyu-zheng <zcy9998@gmail.com>
2018-10-08 23:31:18 -0500

    Add Code generating private.config template

--- BugTracker/Global.asax.cs
+++ BugTracker/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Xml;
 
 namespace BugTracker
 {
@@ -16,6 +17,54 @@ namespace BugTracker
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+            // Generate default private.config template
+            if (!System.IO.File.Exists(Server.MapPath("~/private.config")))
+            {
+                XmlWriterSettings settings = new XmlWriterSettings()
+                {
+                    Indent = true,
+                    IndentChars = "  "
+                };
+                using (XmlWriter writer = XmlWriter.Create(Server.MapPath("~/private.config"), settings))
+                {
+                    writer.WriteStartElement("appSettings");
+
+                    writer.WriteStartElement("add");
+                    writer.WriteAttributeString("key", "username");
+                    writer.WriteAttributeString("value", "[smtp username]");
+                    writer.WriteEndElement();
+
+                    writer.WriteStartElement("add");
+                    writer.WriteAttributeString("key", "password");
+                    writer.WriteAttributeString("value", "[smtp password]");
+                    writer.WriteEndElement();
+
+                    writer.WriteStartElement("add");
+                    writer.WriteAttributeString("key", "emailfrom");
+                    writer.WriteAttributeString("value", "[from email address]");
+                    writer.WriteEndElement();
+
+                    writer.WriteStartElement("add");
+                    writer.WriteAttributeString("key", "emailto");
+                    writer.WriteAttributeString("value", "[to email address]");
+                    writer.WriteEndElement();
+
+                    writer.WriteStartElement("add");
+                    writer.WriteAttributeString("key", "host");
+                    writer.WriteAttributeString("value", "[smtp host]");
+                    writer.WriteEndElement();
+
+                    writer.WriteStartElement("add");
+                    writer.WriteAttributeString("key", "port");
+                    writer.WriteAttributeString("value", "[smtp port]");
+                    writer.WriteEndElement();
+
+                    writer.WriteEndElement();
+                    writer.Close();
+                }
+            }
         }
     }
 }
