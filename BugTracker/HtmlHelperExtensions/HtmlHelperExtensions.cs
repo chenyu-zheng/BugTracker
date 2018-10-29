f2d6f133ce5408edc0398858a83cfdedf6f57368
chenyu-zheng <zcy9998@gmail.com>
2018-10-29 00:05:02 -0500

    Implement HtmlHelper extension: RawSafe

---
+++ BugTracker/HtmlHelperExtensions/HtmlHelperExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Ganss.XSS;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BugTracker.HtmlHelperExtensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static IHtmlString RawSafe(this HtmlHelper helper, string value)
+        {
+            var sanitizer = new HtmlSanitizer();
+            return MvcHtmlString.Create(sanitizer.Sanitize(value));
+        }
+    }
+}
